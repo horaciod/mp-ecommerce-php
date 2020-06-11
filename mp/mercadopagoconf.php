ee1fc01218ced4a11c734a56947d02602f423795
Horacio Degiorgi <horaciod@gmail.com>
2020-06-10 23:54:07 -0300

    mp/mercadopagoconf: cambio en la URL "falto la barra!!"

--- mp/mercadopagoconf.php
+++ mp/mercadopagoconf.php
@@ -2,7 +2,7 @@
 //require $conf['dirsistema'].'include_local/vendor/mercadopago/sdk/lib/mercadopago.php';
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-require __DIR__  . '../vendor/autoload.php';
+require __DIR__  . '/../vendor/autoload.php';
 class mp_local
 {
     private $access_token = 'APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398';
