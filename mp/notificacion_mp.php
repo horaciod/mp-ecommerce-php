41eca69f54d188a2857ff214f626c899bbd7462f
Horacio Degiorgi <horaciod@gmail.com>
2020-06-11 10:45:36 -0300

    cambio en el log

--- mp/notificacion_mp.php
+++ mp/notificacion_mp.php
@@ -1,14 +1,14 @@
 <?php
-require './mercadopagoconf.php' ;
-    $mp   = new mp_local();
-   if (isset($_POST['type']) and isset($_POST['id'])) {
-        $jsonSent = $mp->process_notification($_POST['type'], $_POST['id']); 
-        file_put_contents("php://stderr", "Notificacion! $jsonSent \n");
-   }
-//"/mp/notificacion_mp.php?data.id=7054169396&type=payment
-var_dump($_GET) ;
-if (isset($_GET['type']) and isset($_GET['data_id'])) {
-    echo "si" ;    
-     $jsonSent = $mp->process_notification($_GET['type'], $_GET['data_id']); 
-    var_dump($jsonSent); 
-}
+// require './mercadopagoconf.php' ;
+//     $mp   = new mp_local();
+//    if (isset($_POST['type']) and isset($_POST['id'])) {
+//         $jsonSent = $mp->process_notification($_POST['type'], $_POST['id']); 
+//         file_put_contents("php://stderr", "Notificacion! $jsonSent \n");
+//    }
+
+
+
+error_log("===========  POST  ============== ".print_r($_POST, true));
+error_log("===========  REQUEST  ============== ".print_r($_REQUEST, true));
+$entityBody = file_get_contents('php://input');
+error_log("===========  TODO  ============== ".print_r($entityBody, true));
